4bae569e41f12a1adf91e23084e9f57f2bdd9913
Wojciech Dlugosz <webmaster@styronet.pl>
2024-06-22 14:19:38 +0200

    Instance for each product

--- dist/js/script.js
+++ dist/js/script.js
@@ -61,8 +61,15 @@ const select = {
 
   const app = {
     initMenu: function(){
-      const testProduct = new Product();
-      console.log('testProduct:', testProduct)
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+    initData: function(){
+      const thisApp = this;
+      thisApp.data = dataSource;
     },
     init: function(){
       const thisApp = this;
@@ -72,6 +79,7 @@ const select = {
       console.log('settings:', settings);
       console.log('templates:', templates);
 
+      thisApp.initData();
       thisApp.initMenu();
     },
   };

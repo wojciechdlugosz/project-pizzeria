060d1be1d9a0b5ef9132f564be90c0ce37de17d5
Wojciech Dlugosz <webmaster@styronet.pl>
2024-06-22 14:46:17 +0200

    Product rendering

--- src/js/script.js
+++ src/js/script.js
@@ -57,7 +57,24 @@ const select = {
       const thisProduct = this;
       thisProduct.id = id;
       thisProduct.data = data;
+      thisProduct.renderInMenu();
       console.log('new Product:', thisProduct);
+    }
+    renderInMenu(){
+      const thisProduct = this;  
+
+      /*generate HTML based on template*/
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      //console.log(generatedHTML);
+
+      /*create element using utils.createElementFromHTML*/
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      /*find menu container*/
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      /*add element to menu*/
+      menuContainer.appendChild(thisProduct.element);
     }
   }
 

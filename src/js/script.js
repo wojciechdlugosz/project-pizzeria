ef71a25293f20f397b926685770393bb8b54e4b5
Wojciech Dlugosz <webmaster@styronet.pl>
2024-06-22 14:27:27 +0200

    Saving constructor arguments

--- src/js/script.js
+++ src/js/script.js
@@ -53,8 +53,10 @@ const select = {
   };
 
   class Product {
-    constructor() {
+    constructor(id, data) {
       const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
       console.log('new Product:', thisProduct);
     }
   }

8ad802eae88596e84dd8e8245216be631e137b42
Wojciech Dlugosz <webmaster@styronet.pl>
2024-06-27 17:40:00 +0200

    Add class Cart

--- src/js/script.js
+++ src/js/script.js
@@ -324,6 +324,21 @@
     }
   }
 
+  class cart{
+    constructor(element){
+      const thisCart = this;
+      thisCart.products = [];
+      thisCart.getElements(element);
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;  
+    }
+  }
+
   const app = {
     initMenu: function(){
       const thisApp = this;

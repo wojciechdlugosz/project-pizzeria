015cd03864cfa0dacca8d3e5385130a0505a9775
Wojciech Dlugosz <webmaster@styronet.pl>
2024-06-27 17:55:33 +0200

    Add cart toggle

--- dist/js/script.js
+++ dist/js/script.js
@@ -329,13 +329,23 @@
       const thisCart = this;
       thisCart.products = [];
       thisCart.getElements(element);
+      thisCart.initActions();
       console.log('new Cart', thisCart);
     }
 
     getElements(element){
       const thisCart = this;
       thisCart.dom = {};
-      thisCart.dom.wrapper = element;  
+      thisCart.dom.wrapper = element; 
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger); 
+    }
+
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(){
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
     }
   }
 

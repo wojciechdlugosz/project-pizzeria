ee258b72c723b9e8ec4dee1b5d2582a87c5b38c5
Wojciech Dlugosz <webmaster@styronet.pl>
2024-06-30 17:04:27 +0200

    Prepare connecting with API

--- src/js/script.js
+++ src/js/script.js
@@ -565,12 +565,32 @@
       const thisApp = this;
       //console.log('thisApp.data:', thisApp.data);
       for(let productData in thisApp.data.products){
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
     initData: function(){
       const thisApp = this;
-      thisApp.data = dataSource;
+
+      thisApp.data = {};
+
+      const url = settings.db.url + '/' + settings.db.products;
+
+      fetch(url)
+        .then(function(rawResponse){
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+
+          /* save parsedResponse as thisApp.data.products */
+          thisApp.data.products = parsedResponse;
+
+          /* execute initMenu method */
+          thisApp.initMenu();
+        });
+
+        console.log('thisApp.data', JSON.stringify(thisApp.data));
+
     },
     initCart: function(){
       const thisApp = this;
@@ -587,7 +607,6 @@
       console.log('templates:', templates);
 
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
     },
   };

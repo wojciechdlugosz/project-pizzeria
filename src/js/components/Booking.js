66a1ebbbfecaf2d559f0cc952b9125544fa52993
Wojciech Dlugosz <webmaster@styronet.pl>
2024-07-10 14:21:31 +0200

    Use date and hour picker widgets

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,6 +1,8 @@
 import {templates, select} from "../settings.js";
-import {utils} from "../utils.js";
 import AmountWidget from "./AmountWidget.js";
+import DatePicker from "./DatePicker.js";
+import HourPicker from "./HourPicker.js";
+import {utils} from "../utils.js";
 
 class Booking{
     constructor(element){
@@ -23,6 +25,8 @@ class Booking{
 
         thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
         thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+        thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+        thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
 
     }
 
@@ -31,6 +35,8 @@ class Booking{
 
         thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
         thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+        thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+        thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
 
         thisBooking.dom.peopleAmount.addEventListener('updated', function(){
         });
